461379f513c1036aaf9fc7240d958767051ddf84
Alexia Adediran <alexiaadediran30@gmail.com>
2020-02-20 13:15:23 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,3 +3,12 @@
 # def my_find(collection)
 
 # end
+require 'pry'
+
+def my_find(collection)
+  i = 0
+  while i < collection.length
+    return(collection[i]) if yield(collection[i])
+    i += 1
+  end
+end
